6b0bff273c94e5ba52f0794ac8edb39d101b1089
Nguyen Vu Truong Huy <nguyenvutruonghuy112@gmail.com>
2024-10-24 13:14:30 +0700

    Court_Owner: Screen

--- app/src/main/java/com/example/prm392_team333_courtbooking/activities/court_owner_layout.java
+++ app/src/main/java/com/example/prm392_team333_courtbooking/activities/court_owner_layout.java
@@ -1,10 +1,13 @@
 package com.example.prm392_team333_courtbooking.activities;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,6 +16,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.prm392_team333_courtbooking.CourtListManage;
 import com.example.prm392_team333_courtbooking.R;
+import com.example.prm392_team333_courtbooking.profile.player_profile;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class court_owner_layout extends AppCompatActivity {
@@ -23,19 +27,39 @@ public class court_owner_layout extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_court_owner_layout);
 
-        View appBar = findViewById(R.id.appBarLayout);
+        /*View appBar = findViewById(R.id.appBarLayout);
         ViewCompat.setOnApplyWindowInsetsListener(appBar, (v, insets) -> {
             int topInset = insets.getSystemWindowInsetTop();
             ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
             params.topMargin = topInset;
             v.setLayoutParams(params);
             return insets;
-        });
+        });*/
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         if (savedInstanceState == null) {
             loadFragment(new CourtListManage());
         }
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                if (item.getItemId() == R.id.add) {
+                    selectedFragment = new court_owner_add();
+                } else if (item.getItemId() == R.id.profile) {
+                    selectedFragment = new court_owner_profile();
+                }else if(item.getItemId() == R.id.calendar){
+                    selectedFragment = new court_owner_calendar();
+                }
+
+                if (selectedFragment != null) {
+                    loadFragment(selectedFragment);
+                }
+
+                return true;
+            }
+        });
     }
 
     private void loadFragment(Fragment fragment) {
